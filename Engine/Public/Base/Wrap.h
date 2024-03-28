7a3a0e6747bf185eb612624ed66de6cc759f428d
krovee <krovee.mail@gmail.com>
2024-03-28 16:57:21 +1000

    Added CWrap class.
    Wrap works as a wrapper around pointer to perform more safely and polish checks around it. Wrap doesn't do *anything* with data at pointers.

---
+++ Engine/Public/Base/Wrap.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <Base/NumericTypes.h>
+#include <Base/Templated.h>
+
+namespace Neowise {
+
+    /**
+    * Wrap works as a wrapper around pointer to perform more safely and polish 
+    * checks around it. Wrap doesn't do *anything* with data at pointers. 
+    */
+    template<class T>
+    class NW_API CWrap {
+    public:
+        constexpr CWrap() = default;
+        ~CWrap() = default;
+
+        explicit CWrap(T* p) : _ptr(p) {}
+        CWrap(nullptr_t) : _ptr() {}
+
+        constexpr bool isValid() const {
+            return _ptr != nullptr;
+        }
+
+        constexpr operator bool() const {
+            return _ptr != nullptr;
+        }
+
+        constexpr T& unwrap() {
+            return *_ptr;
+        }
+
+        constexpr const T& unwrap() const {
+            return *_ptr;
+        }
+
+        constexpr operator T&() {
+            return *_ptr;
+        }
+
+        constexpr operator const T&() const {
+            return *_ptr;
+        }
+
+    private:
+        T* _ptr = nullptr;
+    };
+}
